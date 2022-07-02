73d32d173a9b8fcdd15dba6bbffb6efea7fe29d1
e.hasegawa <sh4728061@gmail.com>
2022-07-02 20:40:08 +0900

    最初のコミット

---
+++ SecondProject/SecondProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondProject
+//  
+//  Created by e.hasegawa on 2022/07/02.
+//  
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
